adf93aead98e6f8760c6afacb86a832d2cd76f36
Leo <mml0816@gmail.com>
2018-09-22 16:55:25 -0700

    fix html

--- base/polls/views.py
+++ base/polls/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+import os
+from lxml import html
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    str = "Error."
+    with open(os.getcwd() + r'\public\index.html', "r") as f:
+        str = f.read()
+    return HttpResponse(str)
